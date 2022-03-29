877591106abc71c50e50a699d4eff117540cbaa0
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2022-03-29 16:03:05 -0500

    proyecto base

---
+++ src/main/java/com/example/clase7_1_gtics/Clase71GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase7_1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase71GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase71GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clase7_1_gtics/Clase71GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase7_1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase71GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
